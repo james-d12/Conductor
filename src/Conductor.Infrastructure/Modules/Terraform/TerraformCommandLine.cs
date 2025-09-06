4ee2ff62f0961ceabc081a2abf38efe47d27749f
james <james_d02@protonmail.com>
2025-09-06 17:13:29 +0100

    refactors

--- src/Conductor.Infrastructure/Modules/Terraform/TerraformCommandLine.cs
+++ src/Conductor.Infrastructure/Modules/Terraform/TerraformCommandLine.cs
@@ -12,35 +12,27 @@ public interface ITerraformCommandLine
 
 public sealed class TerraformCommandLine : ITerraformCommandLine
 {
-    public async Task<CommandLineResult> RunTerraformJsonOutput(string executeDirectory)
-    {
-        return await new CommandLineBuilder("terraform-config-inspect")
+    public async Task<CommandLineResult> RunTerraformJsonOutput(string executeDirectory) =>
+        await new CommandLineBuilder("terraform-config-inspect")
             .WithArguments("--json .")
             .WithWorkingDirectory(executeDirectory)
             .ExecuteAsync();
-    }
 
-    public async Task<CommandLineResult> RunInitAsync(string executeDirectory)
-    {
-        return await new CommandLineBuilder("terraform")
+    public async Task<CommandLineResult> RunInitAsync(string executeDirectory) =>
+        await new CommandLineBuilder("terraform")
             .WithArguments("init")
             .WithWorkingDirectory(executeDirectory)
             .ExecuteAsync();
-    }
 
-    public async Task<CommandLineResult> RunValidateAsync(string executeDirectory)
-    {
-        return await new CommandLineBuilder("terraform")
+    public async Task<CommandLineResult> RunValidateAsync(string executeDirectory) =>
+        await new CommandLineBuilder("terraform")
             .WithArguments("validate")
             .WithWorkingDirectory(executeDirectory)
             .ExecuteAsync();
-    }
 
-    public async Task<CommandLineResult> RunPlanAsync(string executeDirectory)
-    {
-        return await new CommandLineBuilder("terraform")
+    public async Task<CommandLineResult> RunPlanAsync(string executeDirectory) =>
+        await new CommandLineBuilder("terraform")
             .WithArguments("plan -input=false -out=plan.tfplan")
             .WithWorkingDirectory(executeDirectory)
             .ExecuteAsync();
-    }
 }
