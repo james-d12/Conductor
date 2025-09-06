2eaa58b65edc1e33c27207fa2883fbb0f04be577
james <james_d02@protonmail.com>
2025-09-06 17:42:51 +0100

    remove unused localfile

--- src/Conductor.Infrastructure/Common/LocalFile.cs
+++
@@ -1,8 +0,0 @@
-namespace Conductor.Infrastructure.Common;
-
-public sealed record LocalFile
-{
-    public required string Name { get; init; }
-    public required string Directory { get; init; }
-    public required string FullPath { get; init; }
-}
