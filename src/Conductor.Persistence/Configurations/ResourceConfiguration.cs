30f19f554ccf22bf6045da3241d06b44254cc381
james <james_d02@protonmail.com>
2025-09-26 23:08:59 +0100

    format

--- src/Conductor.Persistence/Configurations/ResourceConfiguration.cs
+++ src/Conductor.Persistence/Configurations/ResourceConfiguration.cs
@@ -38,7 +38,7 @@ internal sealed class ResourceConfiguration : IEntityTypeConfiguration<Resource>
                 id => id.Value,
                 value => new ResourceTemplateId(value)
             ).IsRequired();
-        
+
         builder.PrimitiveCollection(b => b.Name).IsRequired();
         builder.Property(b => b.CreatedAt).IsRequired().HasDefaultValueSql("now()");
         builder.Property(b => b.UpdatedAt).IsRequired().HasDefaultValueSql("now()");
