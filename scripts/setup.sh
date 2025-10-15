a5f6acbb112bc1a57abef705e1b45463c9cfe836
james <james_d02@protonmail.com>
2025-10-15 22:22:07 +0100

    go back to .net 9 for now

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/sh
 
-dotnet tool install --global dotnet-ef --version 10.0.0-rc.1.25451.107
-dotnet tool install --global docfx --version 2.78.3
+dotnet tool install --global dotnet-ef --version 9.0.306
