b70a2f58c43e5437d9ac6a9ba1a2ad9507a2f94f
james <james_d02@protonmail.com>
2025-11-19 18:44:35 +0000

    Added ResourceInstance as a domain concept, This will encompass a provisioned resource.

---
+++ engine/src/Conductor.Engine.Domain/ResourceInstance/ResourceInstanceState.cs
@@ -0,0 +1,7 @@
+namespace Conductor.Engine.Domain.ResourceInstance;
+
+public sealed record ResourceInstanceState
+{
+    public required Uri Location { get; init; }
+    public string? Workspace { get; init; }
+}
